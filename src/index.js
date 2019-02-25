4e92406b9b90cecccef48fbfc4bf10562c25c579
Piotr-Setsko <lihadzey@gmail.com>
2019-02-25 17:31:43 +0300

    Tried to implement function check(str, brackets Config)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let brackets = bracketsConfig.split('');
+  brackets = brackets.sort();
+  for (let i=0; i<brackets.length; i++) {
+    if ((brackets[i] == "'") || (brackets[i] == ",")|| (brackets[i] == " ")) {
+      brackets.splice(i, 1);
+        }
+    }
 }
