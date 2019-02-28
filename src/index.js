9569c3cdc380a669835d41556fa4506742117b7a
Piotr-Setsko <lihadzey@gmail.com>
2019-02-28 17:22:53 +0300

    Implemented function check(str, bracketsConfig) which checks the brackets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  let brackets = bracketsConfig.split('');
-  brackets = brackets.sort();
-  for (let i=0; i<brackets.length; i++) {
-    if ((brackets[i] == "'") || (brackets[i] == ",")|| (brackets[i] == " ")) {
-      brackets.splice(i, 1);
+  let arr = [];
+  for (let i = 0; i<str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++ ) {
+      if (str[i] == bracketsConfig[j][1]) {
+        if (bracketsConfig[j][1] == bracketsConfig[j][0]) {
+          if (arr[arr.length - 1] == str[i]) {
+            arr.pop();
+            console.log(arr);
+            continue;
+          } else {
+          arr.push(str[i]);
+          console.log(arr)
+          continue;
         }
+      }
+      if (arr.length == 0) {
+        return false;
+        } else if (arr[arr.length - 1] != bracketsConfig[j][0]) {
+          return false;
+        } else if (arr[arr.length - 1] == bracketsConfig[j][0]) {
+          arr.pop();
+          break;
+        }
+        } else if (str[i] == bracketsConfig[j][0]) {
+          arr.push(str[i]);
+        } else {
+          continue;
+        }
+      }
     }
+    if (arr.length == 0) {
+    return true;
+    } else {
+    return false;
+  }
 }
